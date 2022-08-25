b2a46510871980121898b36e81e54ce75a707c9b
dlcdev1 <david.lcorreia@mercadolivre.com>
2022-08-25 00:29:03 -0300

    add new users

---
+++ couse_udemy/src/test/java/com/example/couse_udemy/CouseUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.couse_udemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouseUdemyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
