17a4e9a04190ccf123031a5486eb91a7a5708f06
dlcdev1 <david.lcorreia@mercadolivre.com>
2022-12-27 16:48:26 -0300

    refactory template

--- pessoal/gastos/src/main/java/com/example/curso/boot/web/controller/BillCollectorController.java
+++ pessoal/gastos/src/main/java/com/example/curso/boot/web/controller/BillCollectorController.java
@@ -5,17 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/funcionarios")
-public class FuncionarioController {
+@RequestMapping("/collectors")
+public class BillCollectorController {
 
     @GetMapping("/cadastrar")
     public String cadastrar() {
-        return "/funcionario/cadastro";
+        return "/collector/cadastro";
     }
 
     @GetMapping("/listar")
     public String listar() {
-        return "/funcionario/lista";
+        return "/collector/lista";
     }
 
 }
