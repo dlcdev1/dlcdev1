a04b0e8457776023ecb77fdb660968d42554142e
dlcdev1 <david.lcorreia@mercadolivre.com>
2023-01-10 19:11:53 -0300

    refactory home

--- pessoal/gastos/src/main/java/com/example/curso/boot/web/controller/HomeController.java
+++ pessoal/gastos/src/main/java/com/example/curso/boot/web/controller/HomeController.java
@@ -1,13 +1,13 @@
 package com.example.curso.boot.web.controller;
 
 import com.example.curso.boot.domains.TimeSource;
+import com.example.curso.boot.services.BillService;
 import com.example.curso.boot.services.TimeSourceService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
-import java.time.Period;
 import java.util.List;
 
 @Controller
@@ -15,15 +15,24 @@ public class HomeController {
 
     private final TimeSourceService timeSourceService;
 
-    public HomeController(TimeSourceService timeSourceService) {
+    private final BillService billService;
+
+    public HomeController(TimeSourceService timeSourceService, BillService billService) {
         this.timeSourceService = timeSourceService;
+        this.billService = billService;
     }
 
     @GetMapping("/")
     public String home(ModelMap model) {
-        model.addAttribute("timeSource", timeSourceService.findAll());
+        model.addAttribute("bill", billService.findAll());
         return "/home";
     }
+
+//    @GetMapping("/listar")
+//    public String listar(ModelMap model) {
+//        model.addAttribute("bill", billService.findAll());
+//        return "/bill/lista";
+//    }
 
     @ModelAttribute("time_source")
     public List<TimeSource> listaDePeriodos() {
